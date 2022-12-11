a52fc03a91f373a66cd99a3fb29334de2ba82357
felixgro <me@felixgrohs.dev>
2022-12-11 12:37:13 +0100

    cleanup

--- resources/views/home.view.php
+++ resources/views/home.view.php
@@ -1,5 +1,3 @@
-<?php // require_once 'partials/head.php' ?>
-
 <?= '<p class="message">PHP output here, potentially large HTML chunks</p>' ?>
 
 <div class="vue-app">
@@ -13,5 +11,3 @@
 </div>
 
 <?= '<p class="message">PHP output here, potentially large HTML chunks</p>' ?>
-
-<?php //require_once 'partials/foot.php' ?>
